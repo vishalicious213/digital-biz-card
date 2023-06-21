0fbe7b5bf6d97580f4f0ce3f161ee8f7a0884110
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:14:00 -0700

    updated /components/About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,8 @@
 export default function About() {
     return (
-        <h1>ABOUT</h1>
+        <>
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </>
     )
 }
