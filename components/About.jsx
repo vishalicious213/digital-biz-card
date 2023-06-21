f04e2a92ea9eb06bed031c16ed46422687dc7a86
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 23:42:23 -0700

    styled About & Interests

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <>
             <h2>About</h2>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="text-detail">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </>
     )
 }
--- components/Interests.jsx
+++ components/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <>
             <h2>Interests</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="text-detail">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </>
     )
 }
