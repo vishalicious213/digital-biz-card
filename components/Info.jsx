d2ed0b4121f51b8ccfaec41c5c533f7dcefb7e67
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:23:32 -0700

    updated /components/Info.jsx

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,5 +1,16 @@
+import vish from "/vish.jpg"
+
 export default function Info() {
     return (
-        <h1>INFO</h1>
+        <>
+            <img src={vish} alt="Vish Singh" />
+            <h1>Vish Singh</h1>
+            <p>Web Developer</p>
+            <p>Portfolio</p>
+            <div>
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
+        </>
     )
 }
