55c226c077e47a69cce96527a2da2f5fd4e6ab27
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:30:06 -0700

    updated /components/Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,15 @@
+import fb from "../public/fb.png"
+import gh from "../public/gh.png"
+import ig from "../public/ig.png"
+import tw from "../public/tw.png"
+
 export default function Footer() {
     return (
-        <h1>FOOTER</h1>
+        <footer>
+            <img className="icon" src={tw} alt="Twitter icon" />
+            <img className="icon" src={fb} alt="Facebook icon" />
+            <img className="icon" src={ig} alt="Instagram icon" />
+            <img className="icon" src={gh} alt="GitHub icon" />
+        </footer>
     )
 }
