bfd214c1b6e2e1ec6e4fe38609a5e079f5139d0a
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 23:47:53 -0700

    added .container to About & Interests

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <>
+        <section className="container">
             <h2>About</h2>
             <p className="text-detail">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-        </>
+        </section>
     )
 }
--- components/Interests.jsx
+++ components/Interests.jsx
@@ -1,8 +1,8 @@
 export default function Interests() {
     return (
-        <>
+        <section className="container">
             <h2>Interests</h2>
             <p className="text-detail">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
-        </>
+        </section>
     )
 }
