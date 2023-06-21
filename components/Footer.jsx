deaecf6aef7c6986858c46ca6e776b1c61d6aefc
vishalicious213 <vishalicious213@gmail.com>
2023-06-21 00:10:09 -0700

    addded links to footer icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,18 @@ import tw from "../public/tw.png"
 export default function Footer() {
     return (
         <footer>
-            <img className="icon" src={tw} alt="Twitter icon" />
-            <img className="icon" src={fb} alt="Facebook icon" />
-            <img className="icon" src={ig} alt="Instagram icon" />
-            <img className="icon" src={gh} alt="GitHub icon" />
+            <a target="blank" href="https://twitter.com/vishalicious213">
+                <img className="icon" src={tw} alt="Twitter icon" />
+            </a>
+            <a target="blank" href="https://www.facebook.com/vish213/">
+                <img className="icon" src={fb} alt="Facebook icon" />
+            </a>
+            <a target="blank" href="https://www.instagram.com/vishalicious213/">
+                <img className="icon" src={ig} alt="Instagram icon" />
+            </a>
+            <a target="blank" href="https://github.com/vishalicious213">
+                <img className="icon" src={gh} alt="GitHub icon" />
+            </a>
         </footer>
     )
 }
