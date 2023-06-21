20154d82193d8e25593a1187a8adb43e2a2ddda8
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 23:34:23 -0700

    added link to email button

--- components/Info.jsx
+++ components/Info.jsx
@@ -13,7 +13,9 @@ export default function Info() {
                     <p className="portfolio">Portfolio</p>
                 </a>
                 <div className="buttons">
-                    <button className="em-btn"><img src={email}/>Email</button>
+                    <a href="mailto:vishalicious213@gmail.com">
+                        <button className="em-btn"><img src={email}/>Email</button>
+                    </a>
                     <a target="blank" href="https://www.linkedin.com/in/vish213/">
                         <button className="li-btn"><img src={linkedin}/>LinkedIn</button>
                     </a>
