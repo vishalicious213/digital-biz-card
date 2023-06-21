9c7c46c0fe57609100a325eefc5071d206246bd1
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:05:57 -0700

    drafted App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Info from "./components/Info"
+import About from "./components/About"
+import Interests from "./components/Interests"
+import Footer from "./components/Footer"
+
+export default function App() {
+    return (
+        <>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </>
+    )
+}
