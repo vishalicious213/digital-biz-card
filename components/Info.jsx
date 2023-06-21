0f78e161c55b698437f1131a1e3f730459971512
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 23:07:32 -0700

    added email & linkedin icons and added to buttons

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,16 +1,20 @@
 import vish from "/vish.jpg"
+import email from "/email.png"
+import linkedin from "/linkedin.png"
 
 export default function Info() {
     return (
-        <section className="info-section">
+        <>
             <img className="profile-pic" src={vish} alt="Vish Singh" />
-            <h1>Vish Singh</h1>
-            <p className="role">Web Developer</p>
-            <p className="portfolio">Portfolio</p>
-            <div>
-                <button>Email</button>
-                <button>LinkedIn</button>
+            <div className="info-container">
+                <h1>Vish Singh</h1>
+                <p className="role">Web Developer</p>
+                <p className="portfolio">Portfolio</p>
+                <div className="buttons">
+                    <button className="em-btn"><img src={email}/>Email</button>
+                    <button className="li-btn"><img src={linkedin}/>LinkedIn</button>
+                </div>
             </div>
-        </section>
+        </>
     )
 }
