66248c27d62f2380a1c536ed07593d9eed0a38df
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:15:45 -0700

    updated /components/Interests.jsx

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -1,5 +1,8 @@
 export default function Interests() {
     return (
-        <h1>INTERESTS</h1>
+        <>
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </>
     )
 }
