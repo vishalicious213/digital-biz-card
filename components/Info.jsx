ebfdf9fd8fb38cf989a11d7f9db8ea21b450e960
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 22:27:19 -0700

    added social media icons to /public

--- components/Info.jsx
+++ components/Info.jsx
@@ -3,7 +3,7 @@ import vish from "/vish.jpg"
 export default function Info() {
     return (
         <>
-            <img src={vish} alt="Vish Singh" />
+            <img className="profile-pic" src={vish} alt="Vish Singh" />
             <h1>Vish Singh</h1>
             <p>Web Developer</p>
             <p>Portfolio</p>
